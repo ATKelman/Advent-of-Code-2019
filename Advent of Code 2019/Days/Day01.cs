909263ded985e56cc7d71999786dfa078b1e0a25
Alexander Kelman <alexanderkelman95@hotmail.com>
2019-12-01 09:01:51 +0100

    Add project files.

---
+++ Advent of Code 2019/Days/Day01.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2019.Days
+{
+    class Day01 : IDay
+    {
+        public string PartOne()
+        {
+            int fuel = 0;
+            File.ReadAllLines("Day01.txt").Select(x => Convert.ToDecimal(x)).ToList().ForEach(x =>
+            {
+                fuel += (int)(Math.Floor(x / 3) - 2);
+            });
+
+            return $"Day 01 Part 1: {fuel}";
+        }
+
+        public string PartTwo()
+        {
+            int fuel = 0;
+            File.ReadAllLines("Day01.txt").Select(x => Convert.ToDecimal(x)).ToList().ForEach(x =>
+            {
+                fuel += GetFuel(x);
+            });
+
+            return $"Day 01 Part 2: {fuel}";
+        }
+
+        private int GetFuel(decimal value)
+        {
+            var fuel = Math.Floor(value / 3) - 2;
+            return (fuel > 0) ? (int)(fuel + GetFuel(fuel)) : 0;
+        }
+    }
+}
---
+++ Advent of Code 2019/Program.cs
@@ -0,0 +1,21 @@
+﻿using Advent_of_Code_2019.Days;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Day = new Day01();
+            Console.WriteLine(Day.PartOne());
+            Console.WriteLine(Day.PartTwo());
+
+            Console.ReadLine();
+        }
+    }
+}
