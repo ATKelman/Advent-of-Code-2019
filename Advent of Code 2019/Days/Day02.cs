d4b3d6a8355700c3e7e8800c94a0214cba6a7f41
Alexander Kelman <alexanderkelman95@hotmail.com>
2019-12-02 06:08:28 +0100

    Day 02 class creation

---
+++ Advent of Code 2019/Days/Day02.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2019.Days
+{
+    public class Day02 : IDay
+    {
+        public string PartOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PartTwo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
