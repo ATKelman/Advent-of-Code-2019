21d0ec40df67059398c4e1e0c343bbbd59a41249
Alexander Kelman <alexander.kelman@cgi.com>
2019-12-12 12:23:20 +0100

    Day12 Setup

---
+++ Advent of Code 2019/Days/Day12.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2019.Days
+{
+	public class Day12 : IDay
+	{
+		public string PartOne()
+		{
+			return $"Day 12 Part 1:";
+		}
+
+		public string PartTwo()
+		{
+			return $"Day 12 Part 2:";
+		}
+	}
+}
--- Advent of Code 2019/Program.cs
+++ Advent of Code 2019/Program.cs
@@ -13,7 +13,7 @@ namespace Advent_of_Code_2019
         {
 			try
 			{
-				var Day = new Day09();
+				var Day = new Day12();
 				Console.WriteLine(Day.PartOne());
 				Console.WriteLine(Day.PartTwo());
 			}
