420fd80c79d5c0afc2d10ffe57268d8562e16b96
Alexander Kelman <alexander.kelman@cgi.com>
2019-12-13 12:02:08 +0100

    Day 11 Part 1 WIP

--- Advent of Code 2019/Days/Day11.cs
+++ Advent of Code 2019/Days/Day11.cs
@@ -10,12 +10,53 @@ namespace Advent_of_Code_2019.Days
 	{
 		public string PartOne()
 		{
-			throw new NotImplementedException();
+			var intComputerInput = System.IO.File.ReadAllText("Day11.txt").Split(',').Select(x => long.Parse(x)).ToArray();
+			var intComputer = new IntComputer(intComputerInput);
+
+			var grid = new Dictionary<(int, int), int>();
+			var currentPos = (0, 0);
+			var input = 0; // 0 - black tile, 1 - white tile 
+			var output = 0;
+			var direction = 0;
+			intComputer.Inputs.Enqueue(input);
+
+			while(intComputer.RunProgram())
+			{
+				output = (int)intComputer.Outputs.Dequeue();
+				if (!grid.ContainsKey(currentPos))
+					grid.Add(currentPos, output);
+				else
+					grid[currentPos] = output;
+
+				direction = (int)intComputer.Outputs.Dequeue();
+
+				switch (direction)
+				{
+					case 0:
+						currentPos = (currentPos.Item1, currentPos.Item2++);
+						break;
+					case 1:
+						currentPos = (currentPos.Item1++, currentPos.Item2);
+						break;
+					case 2:
+						currentPos = (currentPos.Item1, currentPos.Item2--);
+						break;
+					case 3:
+						currentPos = (currentPos.Item1--, currentPos.Item2);
+						break;
+				}
+
+				input = grid.ContainsKey(currentPos) ? grid[currentPos] : 0;
+				intComputer.Inputs.Enqueue(input);
+			}
+
+			var result = grid.Count();
+			return $"Day 11 Part 1: {result}.";
 		}
 
 		public string PartTwo()
 		{
-			throw new NotImplementedException();
+			return $"Day 11 Part 2: ";
 		}
 	}
 }
