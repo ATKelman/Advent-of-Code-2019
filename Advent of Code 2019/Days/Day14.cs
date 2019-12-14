732dada268860af938784195ba21be07e3712448
Alexander Kelman <alexanderkelman95@hotmail.com>
2019-12-14 18:02:36 +0100

    Day 14 Part 1

--- Advent of Code 2019/Days/Day14.cs
+++ Advent of Code 2019/Days/Day14.cs
@@ -10,7 +10,25 @@ namespace Advent_of_Code_2019.Days
     {
         public string PartOne()
         {
-            return $"Day 14 Part 1";
+            var input = System.IO.File.ReadAllText("Day14.txt").Split(',').Select(x => long.Parse(x)).ToArray();
+            var intComputer = new IntComputer(input);
+
+            intComputer.RunProgram();
+
+            var grid = new Dictionary<(long, long), long>();
+            while (intComputer.Outputs.Count() > 0)
+            {
+                var x = intComputer.Outputs.Dequeue();
+                var y = intComputer.Outputs.Dequeue();
+                var id = intComputer.Outputs.Dequeue();
+
+                if (!grid.ContainsKey((x, y)))
+                {
+                    grid.Add((x, y), id);
+                }
+            }
+
+            return $"Day 14 Part 1 {grid.Values.Where(x => x == 2).Count()}.";
         }
 
         public string PartTwo()
