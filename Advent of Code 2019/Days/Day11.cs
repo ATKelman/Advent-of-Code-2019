54f334e2e2b1da4978ba94d15e39ecc86dd32f6c
Alexander Kelman <alexanderkelman95@hotmail.com>
2019-12-14 17:47:42 +0100

    Day 11 Part 1 & 2 Complete

--- Advent of Code 2019/Days/Day11.cs
+++ Advent of Code 2019/Days/Day11.cs
@@ -10,53 +10,78 @@ namespace Advent_of_Code_2019.Days
 	{
 		public string PartOne()
 		{
-			var intComputerInput = System.IO.File.ReadAllText("Day11.txt").Split(',').Select(x => long.Parse(x)).ToArray();
-			var intComputer = new IntComputer(intComputerInput);
-
-			var grid = new Dictionary<(int, int), int>();
-			var currentPos = (0, 0);
-			var input = 0; // 0 - black tile, 1 - white tile 
-			var output = 0;
-			var direction = 0;
-			intComputer.Inputs.Enqueue(input);
-
-			while(intComputer.RunProgram())
-			{
-				output = (int)intComputer.Outputs.Dequeue();
-				if (!grid.ContainsKey(currentPos))
-					grid.Add(currentPos, output);
-				else
-					grid[currentPos] = output;
-
-				direction = (int)intComputer.Outputs.Dequeue();
-
-				switch (direction)
-				{
-					case 0:
-						currentPos = (currentPos.Item1, currentPos.Item2++);
-						break;
-					case 1:
-						currentPos = (currentPos.Item1++, currentPos.Item2);
-						break;
-					case 2:
-						currentPos = (currentPos.Item1, currentPos.Item2--);
-						break;
-					case 3:
-						currentPos = (currentPos.Item1--, currentPos.Item2);
-						break;
-				}
-
-				input = grid.ContainsKey(currentPos) ? grid[currentPos] : 0;
-				intComputer.Inputs.Enqueue(input);
-			}
-
-			var result = grid.Count();
+			var result = GetPaintedGrid(0).Count();
 			return $"Day 11 Part 1: {result}.";
 		}
 
 		public string PartTwo()
 		{
-			return $"Day 11 Part 2: ";
+            var grid = GetPaintedGrid(1);
+
+            for (int y = grid.Keys.Max(i => i.Item2); y >= grid.Keys.Min(i => i.Item2); y--)  
+            {
+                var line = "";
+                for (int x = grid.Keys.Min(i => i.Item1); x <= grid.Keys.Max(i => i.Item1); x++)
+                {
+                    if (grid.ContainsKey((x, y)))
+                    {
+                        if (grid[(x, y)] == 1) line += "#";
+                        else line += " ";
+                    }
+                    else line += " ";
+                }
+                Console.WriteLine(line);
+            }
+
+            return $"Day 11 Part 2: See Console Above.";
 		}
+
+        private Dictionary<(int, int), int> GetPaintedGrid(int firstTile)
+        {
+            var intComputerInput = System.IO.File.ReadAllText("Day11.txt").Split(',').Select(x => long.Parse(x)).ToArray();
+            var intComputer = new IntComputer(intComputerInput);
+
+            var grid = new Dictionary<(int, int), int>();
+            var currentPos = (0, 0);
+            var input = firstTile; // 0 - black tile, 1 - white tile 
+            var output = 0;
+            var direction = 0;
+            intComputer.Inputs.Enqueue(input);
+
+            while (intComputer.RunProgram())
+            {
+                output = (int)intComputer.Outputs.Dequeue();
+                if (!grid.ContainsKey(currentPos))
+                    grid.Add(currentPos, output);
+                else
+                    grid[currentPos] = output;
+
+                var outputDirection = (int)intComputer.Outputs.Dequeue();
+
+                direction = (outputDirection == 0) ? direction - 1 : direction + 1; // 0 - turn left, 1 - turn right 
+                direction = (direction + 4) % 4;
+
+                switch (direction)
+                {
+                    case 0:
+                        currentPos = (currentPos.Item1, currentPos.Item2 + 1);
+                        break;
+                    case 1:
+                        currentPos = (currentPos.Item1 + 1, currentPos.Item2);
+                        break;
+                    case 2:
+                        currentPos = (currentPos.Item1, currentPos.Item2 - 1);
+                        break;
+                    case 3:
+                        currentPos = (currentPos.Item1 - 1, currentPos.Item2);
+                        break;
+                }
+
+                input = grid.ContainsKey(currentPos) ? grid[currentPos] : 0;
+                intComputer.Inputs.Enqueue(input);
+            }
+
+            return grid;
+        }
 	}
 }
