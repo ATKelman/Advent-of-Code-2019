8a641a21b5b6107c820ecedbcbd9fc18334b743c
Alexander Kelman <alexanderkelman95@hotmail.com>
2019-12-14 23:51:17 +0100

    Day 13 Part 2 - Process outputs after program run is complete ....

--- Advent of Code 2019/Days/Day13.cs
+++ Advent of Code 2019/Days/Day13.cs
@@ -10,7 +10,7 @@ namespace Advent_of_Code_2019.Days
     {
         public string PartOne()
         {
-            var input = System.IO.File.ReadAllText("Day14.txt").Split(',').Select(x => long.Parse(x)).ToArray();
+            var input = System.IO.File.ReadAllText("Day13.txt").Split(',').Select(x => long.Parse(x)).ToArray();
             var intComputer = new IntComputer(input);
 
             intComputer.RunProgram();
@@ -33,25 +33,41 @@ namespace Advent_of_Code_2019.Days
 
         public string PartTwo()
         {
-            var input = System.IO.File.ReadAllText("Day14.txt").Split(',').Select(x => long.Parse(x)).ToArray();
+            var input = System.IO.File.ReadAllText("Day13.txt").Split(',').Select(x => long.Parse(x)).ToArray();
             input[0] = 2; // Play for free
 
             var intComputer = new IntComputer(input);
-
-            intComputer.RunProgram();
-            long playerScore = 0;
             var grid = new Dictionary<(long, long), long>();
+            while (intComputer.RunProgram())
+            {
+                ProcessOutputs(ref grid, ref intComputer);
+
+                var ball = grid.Where(x => x.Value == 4).SingleOrDefault();
+                var paddle = grid.Where(x => x.Value == 3).SingleOrDefault();
+
+                if (ball.Key.Item1 > paddle.Key.Item1)
+                    intComputer.Inputs.Enqueue(1);
+                else if (ball.Key.Item1 < paddle.Key.Item1)
+                    intComputer.Inputs.Enqueue(-1);
+                else
+                    intComputer.Inputs.Enqueue(0);
+            }
+
+            ProcessOutputs(ref grid, ref intComputer);
+
+            var value = grid[(-1, 0)];
+            return $"Day 14 Part 2: {value}";
+        }
+
+        private void ProcessOutputs(ref Dictionary<(long, long), long> grid, ref IntComputer intComputer)
+        {
             while (intComputer.Outputs.Count() > 0)
             {
                 var x = intComputer.Outputs.Dequeue();
                 var y = intComputer.Outputs.Dequeue();
                 var id = intComputer.Outputs.Dequeue();
 
-                if (x == -1 && y == 0)
-                {
-                    playerScore = id;
-                }
-                else if (!grid.ContainsKey((x, y)))
+                if (!grid.ContainsKey((x, y)))
                 {
                     grid.Add((x, y), id);
                 }
@@ -60,8 +76,6 @@ namespace Advent_of_Code_2019.Days
                     grid[(x, y)] = id;
                 }
             }
-
-            return $"Day 14 Part 2: {playerScore}";
         }
     }
 }
