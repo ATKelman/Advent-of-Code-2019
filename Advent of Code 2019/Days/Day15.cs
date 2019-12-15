d98582d01da5b7d7ff048003317be4cd3d4a55bf
Alexander Kelman <alexanderkelman95@hotmail.com>
2019-12-15 20:27:19 +0100

    Day 15 WIP

--- Advent of Code 2019/Days/Day15.cs
+++ Advent of Code 2019/Days/Day15.cs
@@ -10,7 +10,43 @@ namespace Advent_of_Code_2019.Days
     {
         public string PartOne()
         {
+            var input = System.IO.File.ReadAllText("Day13.txt").Split(',').Select(x => long.Parse(x)).ToArray();
+            var intComputer = new IntComputer(input);
+
+            (int, int) currentPos = (0, 0);
+            (int, int) previousPos = currentPos;
+            (int, int)[] directions = new (int, int)[]
+            {
+                (0, 1), 
+                (1, 0), 
+                (0, -1),
+                (-1, 0)
+            }; // array pos + 1 
+            var grid = new Dictionary<(int x, int y), int>();
+
+            // Give input
+            // while not oxygen found 
+            // while int computer 
+            while (true)
+            {
+                var index = 0;
+                if (grid.ContainsKey(AddTuple2(currentPos, directions[index])) || directions[index] == previousPos)
+                    index++;
+
+                intComputer.RunProgram();
+            }
+
             return $"Day 15 Part 1:";
+        }
+
+        private (int, int) AddTuple2((int, int) first, (int, int) second)
+        {
+            return (first.Item1 + second.Item1, first.Item2 + second.Item2);
+        }
+
+        private int GetNextDirection(int index)
+        {
+
         }
 
         public string PartTwo()
